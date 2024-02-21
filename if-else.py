47d77cf5ee5cd21bd39dcfa10214256f6a15d6c8
Mojtaba-Rajaei <Mojtabaraj@outlook.com>
2024-02-21 11:34:18 -0500

    use control flow

---
+++ if-else.py
@@ -0,0 +1,10 @@
+inputUser = int(input("Please Enter the right word: "))
+
+if inputUser == 0:
+    print("hello ", inputUser + 10)
+elif inputUser == 1:
+    print("hello " , inputUser + 1)
+elif inputUser == 2:
+    print("hello" , inputUser * 2)
+else:
+    print("hello world")
